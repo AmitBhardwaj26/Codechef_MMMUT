8bf2a692ccb240cef1e10cdb1a39578d5c1d45c9
Amit Bhardwaj <93838323+AmitBhardwaj26@users.noreply.github.com>
2023-07-01 21:16:42 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -6,6 +6,7 @@ mongoose.set('strictQuery', true); // for strictquery warning
 const dotenv = require('dotenv'); 
 dotenv.config(); 
 
+// for succesful connection
 const dbConnection = async() => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {  //process.env.MONGO_URL
